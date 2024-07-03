10f5201eefe02e637885e94c25fbbd2ed0c546e2
zach colburn <colburn.zach7@gmail.com>
2024-07-02 19:17:26 -0500

    Figuring out how to make lists with C#

--- Program.cs
+++ Program.cs
@@ -1,11 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
-List<string> products = new List<string>()
+using System.Reflection.Metadata.Ecma335;
+
+List<Product> products = new List<Product>()
 {
-    "Football",
-    "Hockey Stick",
-    "Boomerang",
-    "Frisbee",
-    "Golf Putter"
+    new Product()
+    {
+        Name = "Football",
+        Price = 15,
+        Sold = false
+    },
+    new Product()
+    {
+        Name = "Hockey Stick",
+        Price = 12,
+        Sold = false
+    },
+    new Product()
+    {
+        Name = "Boomerang",
+        Price = 8,
+        Sold = false
+    },
+    new Product()
+    {
+        Name = "Frisbee",
+        Price = 8,
+        Sold = false
+    },
+    new Product()
+    {
+        Name = "Golf Putter",
+        Price = 35,
+        Sold = false
+    },
 };
 
 string greeting = @"Welcome to Thrown for a Loop
@@ -17,7 +44,7 @@ Console.WriteLine(@"Products:");
 
 for (int i = 0; i < products.Count; i++)
 {
-    Console.WriteLine($"{i + 1}. {products[i]}");
+    Console.WriteLine($"{i + 1}. {products[i].Name}");
 }
 
 Console.WriteLine("Please enter a product number: ");
@@ -28,7 +55,9 @@ int response = int.Parse(Console.ReadLine().Trim());
 while (response > products.Count || response < 1)
 {
   Console.WriteLine("Choose a number between 1 and 5!");
+
   response = int.Parse(Console.ReadLine().Trim());
 }
 
-Console.WriteLine($"You chose: {products[response - 1]}");
+Product chosenProduct = products[response - 1];
+Console.WriteLine($"You chose: {chosenProduct.Name}, which costs {chosenProduct.Price} dollars and is {(chosenProduct.Sold ? "" : "not ")}sold.");
